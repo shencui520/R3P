1bc5cf0b67455ef56b36d55f71ca44aa7e0865b2
shencui520 <74359805+shencui520@users.noreply.github.com>
2021-02-23 13:22:48 +0800

    Update diy-smartdns.sh

--- diy-smartdns.sh
+++ diy-smartdns.sh
@@ -1,3 +1,16 @@
+
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
 WORKINGDIR="`pwd`/feeds/packages/net/smartdns"
 mkdir $WORKINGDIR -p
 rm $WORKINGDIR/* -fr
